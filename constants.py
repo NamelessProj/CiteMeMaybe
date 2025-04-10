056fc0426f951de570ad8c5161f3f40d7b9e0ce4
Kevin <pintokevin2002@hotmail.com>
2025-04-10 23:28:47 +0200

    Adding some constants

---
+++ constants.py
@@ -0,0 +1,29 @@
+CONSTANTS = {
+    "no_saving": "`no-saving`",
+    "all_commands": [
+        {
+            "name": "random_citation",
+            "description": "Get a random citation from the database or for a specific user."
+        },
+        {
+            "name": "how_many",
+            "description": "Get the number of citations total or for a specific user."
+        },
+        {
+            "name": "get_a_citation",
+            "description": "Get a citation from the database by ID."
+        },
+        {
+            "name": "setup_server",
+            "description": "Set up the server settings.\n**Only available for administrators.**"
+        },
+        {
+            "name": "update_database",
+            "description": "Update the database with all the messages from the channel.\n**Only available for administrators.**"
+        },
+        {
+            "name": "help",
+            "description": "Get help with the bot commands."
+        }
+    ]
+}
