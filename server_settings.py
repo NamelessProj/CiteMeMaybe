1afd7515d1fe45b70d06e241195c9175e52d682e
Kevin <pintokevin2002@hotmail.com>
2025-04-11 21:29:51 +0200

    Removing all "get_database" to put the database as a parameter of each function

--- server_settings.py
+++ server_settings.py
@@ -1,20 +1,21 @@
-from database import get_database
+from collections.abc import Mapping
+from pymongo.synchronous.database import Database
 
 
-def setup_server_settings(guild_id: int, citation_channel_id: int):
+def setup_server_settings(db: Database[Mapping], guild_id: int, citation_channel_id: int):
     """
     This function sets up the server settings in the database.
+    :param db: The database
     :param guild_id: The ID of the guild
     :param citation_channel_id: The ID of the channel
     :return: The server settings
     """
-    # Getting the database and collection
-    db = get_database()
-    collection = db["server_settings"]
+    # Getting the collection
+    collection = db.get_collection('server_settings')
 
     # Checking if the server settings already exist
     if collection.find_one({"guild_id": guild_id}):
-        return update_server_settings(guild_id, citation_channel_id)
+        return update_server_settings(db, guild_id, citation_channel_id)
 
     # Setting up the server settings
     server_settings = {
@@ -30,9 +31,10 @@ def setup_server_settings(guild_id: int, citation_channel_id: int):
     return server_settings
 
 
-def update_server_settings(guild_id: int, citation_channel_id: int = None):
+def update_server_settings(db: Database[Mapping], guild_id: int, citation_channel_id: int = None):
     """
     This function updates the server settings in the database.
+    :param db: The database
     :param guild_id: The ID of the guild
     :param citation_channel_id: The ID of the channel
     :return: The server settings
@@ -41,9 +43,8 @@ def update_server_settings(guild_id: int, citation_channel_id: int = None):
     if citation_channel_id is None:
         return None
 
-    # Getting the database and collection
-    db = get_database()
-    collection = db["server_settings"]
+    # Getting the collection
+    collection = db.get_collection('server_settings')
 
     # Preparing the update data
     update_data = {}
@@ -61,15 +62,15 @@ def update_server_settings(guild_id: int, citation_channel_id: int = None):
     return server_settings
 
 
-def get_server_settings(guild_id: int):
+def get_server_settings(db: Database[Mapping], guild_id: int):
     """
     This function gets the server settings from the database.
+    :param db: The database
     :param guild_id: The ID of the guild
     :return: The server settings
     """
-    # Getting the database and collection
-    db = get_database()
-    collection = db["server_settings"]
+    # Getting the collection
+    collection = db.get_collection('server_settings')
 
     # Getting the server settings
     server_settings = collection.find_one({"guild_id": guild_id})
