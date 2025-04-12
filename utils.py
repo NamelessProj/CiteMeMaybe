5d2a5fc6520110ad60b5824a3ce7aa06ed4b72b8
Kevin <pintokevin2002@hotmail.com>
2025-04-12 16:30:08 +0200

    Adding some functions to get the top writers and talkers

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import re
+from collections.abc import Mapping
+
 import discord
 
 def replacing_mentions(message: discord.Message):
@@ -135,3 +137,63 @@ def setup_citation_embed(citation):
         "embed": embed,
         "all_mentions_string": all_mentions_string
     }
+
+
+def sort_citations(citations: dict, limit: int = 5):
+    """
+    This function sorts the citations by timestamp.
+    :param citations: The list of citations
+    :param limit: The limit of citations to get
+    :return:
+    """
+    # Sorting the users by citation count
+    sorted_users = sorted(citations.items(), key=lambda x: x[1], reverse=True)
+
+    # The top users
+    return_text = ""
+
+    min_top = min(limit, len(sorted_users))
+
+    for i in range(min_top):
+        user_id, count = sorted_users[i]
+        cit_str = "citations" if count > 1 else "citation"
+        return_text += f"1. <@{user_id}> - {count} {cit_str}\n"
+
+    return return_text
+
+
+def get_top_user_citations_said(limit: int = 5, citations: list = [Mapping]):
+    """
+    This function gets the most citations said by users.
+    :param limit: The limit of citations to get
+    :param citations: The list of citations
+    :return: The top users with the most citations
+    """
+    # Creating a dictionary to store the citation count for each user
+    citation_count = {}
+    for citation in citations:
+        for mention in citation["mentions"]:
+            user_id = mention["id"]
+            if user_id not in citation_count:
+                citation_count[user_id] = 0
+            citation_count[user_id] += 1
+
+    return sort_citations(citation_count, limit)
+
+
+def get_top_user_citations_written(limit: int = 5, citations: list = [Mapping]):
+    """
+    This function gets the most citations written by users.
+    :param limit: The limit of citations to get
+    :param citations: The list of citations
+    :return: The top users with the most citations
+    """
+    # Creating a dictionary to store the citation count for each user
+    citation_count = {}
+    for citation in citations:
+        author_id = citation["author"]["id"]
+        if author_id not in citation_count:
+            citation_count[author_id] = 0
+        citation_count[author_id] += 1
+
+    return sort_citations(citation_count, limit)
